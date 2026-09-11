8ac6416715d969a880043cee20abef01e4b59418
Roberto Lopez <ing.robertolc@gmail.com>
2026-09-10 21:32:43 -0600

    Setup inicial: proyecto Maven con Java 17, Spring Boot 4.1.1 y dependencias base

---
+++ src/main/java/com/pinwox/tvmazeapi/TvMazeApiApplication.java
@@ -0,0 +1,13 @@
+package com.pinwox.tvmazeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvMazeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvMazeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pinwox/tvmazeapi/TvMazeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinwox.tvmazeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvMazeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
